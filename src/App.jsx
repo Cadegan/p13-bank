abd57a98543a16d38391a3a9c74c4105507b8a5b
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-10-10 14:44:26 +0200

    SignIn v0

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import SignIn from "./pages/SignIn/SignIn";
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -13,8 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        {/* <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/user/:id" element={<DashBoard />}></Route>
+        <Route exact path="/login" element={<SignIn />}></Route>
+        {/* <Route exact path="/user/:id" element={<DashBoard />}></Route>
         <Route exact path="*" element={<Error />}></Route> */}
       </Routes>
       <Footer />
