58157092103278f439e8a31e068bbf62094c38f4
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-10-10 11:29:28 +0200

    hero-content : Css Ok 👍🏻

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        {/* <Route exact path="/" element={<Home />}></Route> */}
+        <Route exact path="/" element={<Home />}></Route>
         {/* <Route exact path="/login" element={<Login />}></Route>
         <Route exact path="/user/:id" element={<DashBoard />}></Route>
         <Route exact path="*" element={<Error />}></Route> */}
