753aa5ef4c8ea5861da02bc5b7c065897b755678
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-10-31 18:23:22 +0100

    DashboardWelcome

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,10 @@
-import DashboardHeader from "./dashboard.header";
+import DashboardWelcome from "./dashboard.welcome";
 import DashboardAccounts from "./dashboard.accounts";
 
 export default function Dashboard() {
   return (
     <main className="main bg-dark">
-      <DashboardHeader />
+      <DashboardWelcome />
       <DashboardAccounts />
     </main>
   );
