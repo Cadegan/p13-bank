b66437467dc9d4644e9ff73d2992d9538000cd6a
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-11-04 11:12:16 +0100

    Rebuild "edit name"

--- src/pages/Dashboard/dashboard.header.jsx
+++ src/pages/Dashboard/dashboard.header.jsx
@@ -8,7 +8,11 @@ import { useForm } from "react-hook-form";
 export default function DashboardHeader() {
   const dispatch = useDispatch();
   const [isEditing, setIsEditing] = useState(false);
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   /* Extracting the userData (firstName and lastName) from the Redux store state. */
   const { userData } = useSelector((state) => state.auth);
@@ -26,60 +30,81 @@ export default function DashboardHeader() {
   return (
     <div className="header">
       {isEditing ? (
-        <h1>
-          Edition mode
-          <br />
-          <form>
-            <div className="input-wrapper userInfo-input">
+        <>
+          <h1>Edition mode</h1>
+          <form className="userInfo-wrapper">
+            <div className="firstNameGroup">
               <input
                 type="text"
                 className="userForm-input"
-                id="editFirstName-input"
-                {...register("firstName")}
+                {...register("firstName", { required: true, minLength: 3 })}
+                // placeholder={userData.firstName}
                 defaultValue={userData.firstName}
               />
+              {errors.firstName && (
+                <p className="text-error">Please check the First Name</p>
+              )}
+            </div>
+            <div className="lastNameGroup">
               <input
                 type="text"
                 className="userForm-input "
-                id="editLastName-input"
-                {...register("lastName")}
+                {...register("lastName", { required: true, minLength: 3 })}
+                // placeholder={userData.lastName}
                 defaultValue={userData.lastName}
               />
+              {errors.lastName && (
+                <p className="text-error">Please check the Last Name</p>
+              )}
             </div>
           </form>
-        </h1>
+          <div className="bt-group">
+            <button
+              className="edit-button"
+              type="submit"
+              onClick={handleSubmit(submiForm)}
+            >
+              Save
+            </button>
+            <button className="edit-button" onClick={() => setIsEditing(false)}>
+              Cancel
+            </button>
+          </div>
+        </>
       ) : (
-        <h1>
-          Welcome back
-          <br />
-          {loading ? (
-            <Oval
-              height={38}
-              width={38}
-              color="#00bc77"
-              wrapperStyle={{}}
-              wrapperClass="spinner"
-              visible={true}
-              ariaLabel="oval-loading"
-              secondaryColor="#00bc77"
-              strokeWidth={2}
-              strokeWidthSecondary={2}
-            />
-          ) : (
-            <span className="header-info">
-              <span id="firstName">{userData.firstName}</span>{" "}
-              <span id="lastName">{userData.lastName}</span> !
-            </span>
-          )}
-        </h1>
+        <>
+          <h1>
+            Welcome back
+            <br />
+            {loading ? (
+              <Oval
+                height={38}
+                width={38}
+                color="#00bc77"
+                wrapperStyle={{}}
+                wrapperClass="spinner"
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#00bc77"
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+              />
+            ) : (
+              <span className="header-info">
+                <span id="firstName">{userData.firstName}</span>{" "}
+                <span id="lastName">{userData.lastName}</span> !
+              </span>
+            )}
+          </h1>
+          <button
+            className="edit-button"
+            type="submit"
+            onClick={handleSubmit(submiForm)}
+          >
+            Edit Name
+          </button>
+        </>
       )}
-      <button
-        className="edit-button"
-        type="submit"
-        onClick={handleSubmit(submiForm)}
-      >
-        {isEditing ? "Update Name" : "Edit Name"}
-      </button>
     </div>
   );
 }
